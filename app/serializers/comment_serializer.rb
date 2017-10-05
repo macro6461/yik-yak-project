1bb981463147ef479558e4af6d6c828e8be62bcf
macro6461 <mattcroak718@gmail.com>
2017-10-05 08:46:35 -0400

    serializers and models for comments, users, posts associated properly

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :post_id, :content
+  belongs_to :user
   belongs_to :post
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :content
   belongs_to :user
+  has_many :comments
 end
