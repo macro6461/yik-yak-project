57124805a0c60ea268ffc7a0a9105f2b59609aa9
macro6461 <mattcroak718@gmail.com>
2017-10-06 08:46:59 -0400

    COMMENTS ARE WORKING

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::CommentsController < ApplicationController
+
+
   def index
     @comments = Comment.all
     render json: @comments, status: 200
@@ -10,12 +12,12 @@ class Api::V1::CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.create(comment_params)
+    @comment = Comment.create!(user_id: params["user_id"], post_id: params["post_id"], content: params["content"])
     render json: @comment, status: 201
   end
 
   private
   def comment_params
-    params.permit(:user_id, :post_id, :content)
+    params.require(:comment).permit(:user_id, :post_id, :content)
   end
 end
