33773c200c4cc9bf3f5c86ac4498026bf7d8c533
Matthew Croak <matthewcroak@Matthews-MBP.home>
2018-01-02 11:35:39 -0500

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
 
-  validates :name, presence: true;
-  validates :username, presence: true
-  validates :username, uniqueness: true
-  validates :email, presence: true
+  # validates :name, presence: true;
+  # validates :username, presence: true
+  # validates :username, uniqueness: true
+  # validates :email, presence: true
 end
